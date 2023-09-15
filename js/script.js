a283b9c0f9d13b79b37d64cbbd418c66a5e5e361
Sebastian Salvadores <sebasalvadores@gmail.com>
2023-09-15 15:02:29 -0300

    Create getComputerChoice()

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function getComputerChoice(){
+    const computerChoice = Math.floor(Math.random() * 3) + 1;
+    if(computerChoice === 1) return 'Rock';
+    else if (computerChoice === 2) return 'Paper';
+    else return 'Scissors';
+}
